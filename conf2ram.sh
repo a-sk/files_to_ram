eb4f3936baeafbc42a64f5aca6bddbf7374b5ace
Skurihin Aleksandr <ahtungster@gmail.com>
2010-09-24 19:41:28 +0400

    improoved messeging system

--- conf2ram.sh
+++ conf2ram.sh
@@ -19,13 +19,12 @@ bind_to_hdd () { maybe_mkdir $2; mount --bind $1 $2 }
 # umount both volumes
 umount_all () { umount $1; umount $2 }
 # rm if exists
-maybe_rm () { [ -e $1 ] && run_as $USER "rm -r $1" }
+maybe_rm () { [ -e $1 ] && "rm -r $1" }
 # check if lock exists
 chk_state () { if [ -e $LOCK ] && echo 1 || echo 0 }
 
 # Show error msgs
-already_running() { echo "Alerady started" }
-not_running() { echo "not started yet" }
+err() {echo $@}
 
 #acepts array of real and backup dirs
 start() { typeset -a args;args=($1 $2);
@@ -35,9 +34,9 @@ stop() { typeset -a args;args=($1 $2);
          sync_it $args; umount_all $args; maybe_rm $args[2]}
 
 case $1 in 
-    start) [ $(chk_state) -eq 0 ] && (touch $LOCK; start $CONFIG; start $CACHE) || already_running
+    start) [ $(chk_state) -eq 0 ] && (touch $LOCK; start $CONFIG; start $CACHE) || err "already_running"
         ;;
-    stop) [ $(chk_state) -eq 1 ] && (rm $LOCK; stop $CONFIG; stop $CACHE) || not_running
+    stop) [ $(chk_state) -eq 1 ] && (rm $LOCK; stop $CONFIG; stop $CACHE) || err "not_running"
         ;;
     *) exit 1
 esac
