b13535850f26ed046da0417dc70a8d93383995c5
Skurihin Aleksandr <ahtungster@gmail.com>
2010-10-05 09:38:32 +0400

    code refactor, logic improvment

--- conf2ram.sh
+++ conf2ram.sh
@@ -1,16 +1,26 @@
 #!/bin/zsh
+#TODO"
+# refactor in order to correctly handle state if $2 is presented
 USER="ask"
 UHOME="/home/$USER"
 RAMDIR="$UHOME/.ramdir"
 LOCK="/var/run/conf2ram_lock"
-DIRS_TO_MOUNT=($UHOME/.local/share $UHOME/.config $UHOME/.cache)
+TMPFS_OPTS="noauto,user,exec,uid=1000,gid=1000"
+
+if [ -n $2 ]; then
+    DIRS_TO_MOUNT=$2
+else
+    DIRS_TO_MOUNT=($UHOME/.config/google-chrome $UHOME/.cache/google-chrome $UHOME/.opera)
+fi
 
 # helpers
 
+# check if ramdir is already mounted
+ramdr_is_mounted () { $(mount | grep "tmpfs on ${RAMDIR}" > /dev/null) }
 # run command $2 throm su as user $1
 run_as () { /bin/su "$1" -c "$2" }
-# create dir in tmpfs
-create_ramdir () { maybe_mkdir $RAMDIR; mount -t tmpfs -o defaults,noatime,mode=177 tmpfs $RAMDIR }
+# create dir in tmpfs if not already exists
+maybe_create_ramdir () { ramdr_is_mounted || ( maybe_mkdir $RAMDIR; mount -t tmpfs -o $TMPFS_OPTS tmpfs $RAMDIR ) }
 # fill ramdir with data
 copy_to_ramdir () { run_as $USER "cp -r $1 $RAMDIR" }
 # recursivly rsync 2 dirs
@@ -20,40 +30,32 @@ maybe_mkdir () { [ ! -e $1 ] && run_as $USER "mkdir $1"}
 # wrapper aroung mount --bind
 bind_to_hdd () { maybe_mkdir $2; mount --bind $1 $2 }
 # just a sematnic wrapper
-unbind () { umount $1 }
+unbind () { umount $1 &>dev/null }
 # rm if exists
 maybe_rm () { [ -e $1 ] && rm -r $1 }
 # check if lock exists
-chk_state () { if [ -e $LOCK ] && echo 1 || echo 0 }
-# show error msgs
-err() { echo $@ }
+chk_state () { if [ -e $LOCK ] && exit 1 }
+# show error msgs; and break
+err() { echo $@; exit 1 }
 
 # main logic
 
-start() { create_ramdir
-        for dir in $DIRS_TO_MOUNT
-            ( copy_to_ramdir $dir 
-              bind_to_hdd $RAMDIR/$(basename $dir) $dir )
-        touch $LOCK 
-    }
+start() { maybe_create_ramdir; 
+          for dir in $DIRS_TO_MOUNT
+              (copy_to_ramdir $dir
+              bind_to_hdd $RAMDIR/$(basename $dir) $dir) && touch $LOCK }
 
 stop() { for dir in $DIRS_TO_MOUNT
-            ( unbind $RAMDIR/$(basename $dir)
-              if [ ! $? -eq 0 ]; then
-                  err "$dir is already in use" && exit 1
-              else
-                  sync_it $RAMDIR/$(basename $dir) $dir
-              fi )
-        umount $RAMDIR
-        rm $LOCK
-     }
+            (unbind $RAMDIR/$(basename $dir) && 
+             sync_it $RAMDIR/$(basename $dir) $dir ||
+             err "$dir is already in use") && (umount $RAMDIR; rm $LOCK) }
 
 case $1 in 
     start) 
-        if [ $(chk_state) -eq 0 ] && start || err "already_running"
+        if [ chk_state ];then start; else err "already_running";fi
         ;;
     stop) 
-        if [ $(chk_state) -eq 1 ] && stop || err "not_running"
+        if [ chk_state ];then stop;  else err "not_running";fi
         ;;
     *) exit 1
 esac
